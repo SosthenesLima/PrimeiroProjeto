65eac5ae5b415af4c75dcce5e660b18c5f4eadf6
SosthenesLima <sosthenes.lima@gmail.com>
2025-06-18 17:06:27 -0300

    Ajustando para o dicionário03

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ String mensagemDeBoasVindas = "Boas vindas ao Screen Sound";
 //List<string> listaDasBandas = new List<string> {"U2", "Metallica", "Megadeath" };
 
 Dictionary<string, List<int>> bandasRegistradas = new Dictionary<string, List<int>>();
+bandasRegistradas.Add("Link Park", new List<int> { 10, 8, 6 });
+bandasRegistradas.Add("The Beatles", new List<int>());
 
 
 void ExibirLogo()
